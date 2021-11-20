2a8a5a16bcd0a8e11ba7053ddc892dc004b61056
MarcosSalas <92918284+MarcosSalas@users.noreply.github.com>
2021-11-20 13:01:32 +0100

    a

--- TEMA 4/T04_ARRAY/src/Arrays.java
+++ TEMA 4/T04_ARRAY/src/Arrays.java
@@ -3,7 +3,7 @@ public class Arrays {
     public static void main(String[] args) {
 
         int[]numeros = new int[10];
-//545
+//54
 
         boolean [] aciertos=new boolean[5];
         String [] palabras = new String[3];
@@ -12,6 +12,7 @@ public class Arrays {
         int[]numeroDos= {1,2,3,4,5,6,7,8,9,10};
         int longitudArray =numeroDos.length;
       //  System.out.println(longitudArray);
+        //ASD
         System.out.println(numeroDos[9]);
 
         for (int i =0; i <=numeroDos.length-1;i++){
