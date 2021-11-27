f2259e2955de1a312e77a9fb9bc880d857b95815
MarcosSalas <92918284+MarcosSalas@users.noreply.github.com>
2021-11-27 03:06:54 +0100

    s

--- TEMA 4/T04_ARRAY/src/PracticasEnCasa/ArrayUnidimensionales/Ejercicio6.java
+++ TEMA 4/T04_ARRAY/src/PracticasEnCasa/ArrayUnidimensionales/Ejercicio6.java
@@ -1,19 +1,48 @@
 package PracticasEnCasa.ArrayUnidimensionales;
+import java.util.Scanner;
 
 public class Ejercicio6 {
     public static void main(String[] args) {
 
+        Scanner longiArray= new Scanner(System.in);
+        System.out.println("ingrese la LONGITUD");
+        int lgtudArray=longiArray.nextInt();
 
+        int arrayUser []= new int[lgtudArray];
+
+        for (int i = 0; i < arrayUser.length; i++) {
+            System.out.println("Introduzca los datos del Array");
+            arrayUser[i]= longiArray.nextInt();
+        }
+        for (int i = 0; i < arrayUser.length; i++) {
+
+            if (arrayUser[i]==0) {
+
+
+            }
+        }
     }
-
 }
 
 /*
-(PosicionesArray) Crea una aplicación que:
-
-pida por teclado la longitud de un array
-cree el array de enteros de la longitud introducida
-pida al usuario que introduzca todos los datos del array
-mueva una posición todos los elementos. el elemento de la posición 0 pasa a la posición 1 y así sucesivamente. El elemento que está en la última posición pasa a la primera
+ el elemento de la posición 0 pasa a la posición 1 y así sucesivamente. El elemento que está en la última posición pasa a la primera
 realizar la tarea anterior pero al contrario. El elemento que está en la primera posición pasa a la última y así sucesivamente
+
+
+
+
+
+
+
+pida al usuario que introduzca todos los datos del array mueva una posición todos los elementos.
+
+
+
+
+
+pida por teclado la longitud de un array cree el array de enteros de la longitud introducida
+
+
+
+
  */
