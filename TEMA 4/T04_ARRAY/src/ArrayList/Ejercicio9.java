37d2a71620a8683bd2eb36a5b35b658b1b04fbb1
MarcosSalas <92918284+MarcosSalas@users.noreply.github.com>
2021-12-03 08:39:27 +0100

    S

--- TEMA 4/T04_ARRAY/src/ArrayList/Ejercicio9.java
+++ TEMA 4/T04_ARRAY/src/ArrayList/Ejercicio9.java
@@ -7,45 +7,49 @@ public class Ejercicio9 {
     public static void main(String[] args) {
         char []abecedario= {'a','b','c','d','e','f','g','h','i','j','k','l','m','ñ','o','p','q','r','s','t','u','v','w','y','z'};
 
-        char abecedario1 = 's';
+
         Scanner pIntroducida = new Scanner(System.in);
-        int longitud = 0;
-        String[] arrayString = new String[longitud];
+        int longitud =0;
+        //String[] arrayString = new String[longitud];   **PORQUE ASI NO,SI PIDE CREAR UN ARRAY STRING
         int opcion = 0;
 
-        do {
+
+        //igrese la longitud
             System.out.println("Cuantas palabras quieres guardar");
-            System.out.println("1 longitud de palabra 1");
-            System.out.println("2 longitud de palabra 2");
-            System.out.println("3 longitud de palabra 3");
+             longitud=pIntroducida.nextInt();
+
+             //luego de obtener la longitud, generar la palabra con las letras del array 1 del tamaño ya indicado(COMO)?
+
+        //luego de generar esa palabra se guarda en el array 2, CREO Q POR ESO SE HACEN 2 FOR ANIDADOS
 
 
-            switch (opcion) {
-                case 1:
-                    System.out.println("Cual es la longitud de la palabra 1");
-                    longitud=pIntroducida.nextInt();
-                    char abecedario1=abecedario[(int)(Math.random()* abecedario.length)];//el match random seria int x q es una posicion no un char
+   //para formar (Rellenar el array) palabras aleatoreas y ponerlas en una posición lo tengo que hacer con un FOR
+        //
+ //    char abecedario1=abecedario[(int)(Math.random()* abecedario.length)];//el match random seria int x q es una posicion no un char
 
+        for (int i = 0; i < longitud; i++) {
+            System.out.println("Palabra " +i);
+            System.out.println("Cuantas letras tiene la palabra");
+            int letras= pIntroducida.nextInt();
+            String palabra ="";   //MISMA PREGUNTA DE ARRIBA
+            for (int j = 0; j < letras; j++) {
+                char letrasAleatoria=abecedario
+                        [(int) (Math.random()*abecedario.length)];
+                //POR CADA ITERACION TOMA UNA LETRA DEL ABECEDARIO , LA GUARDA EN letrasAleatoria, que ese a su vez lo guarda en cada posición del array PALABRA?
+                palabra+=letrasAleatoria;
 
-                    break;
-                case 2:
-                    System.out.println("Cual es la longitud de la palabra 2");
-                    break;
-                case 3:
-                    System.out.println("Cual es la longitud de la palabra 3");
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
             }
+            System.out.println(palabra);
+        }
 
-        } while (opcion != 0);
 
-        System.out.println("Las palabras heredadas son");
-        System.out.println();
-        System.out.println();
-        System.out.println();
+
+
+
+
+
+
+
 
 
     }
