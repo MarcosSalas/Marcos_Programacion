df469d0f2f524a4b2f6ff0bf99289b7e2c26bb57
AlvaroSR-dev <alvarosanchezruiz3@gmail.com>
2021-12-03 12:56:12 +0100

    s

---
+++ TEMA 4/T04_ARRAY/src/ArrayList/Ejercicio12Cham.java
@@ -0,0 +1,43 @@
+package ArrayList;
+
+public class Ejercicio12Cham {
+    public static void main(String[] args) {
+        String[]equipos1=new String[]{"Barcelona","PSG","Bayern","Manchester City","Juventus","RB Leizpzig","Valencia","Liverpool"};
+        String[]equipos2=new String[]{"Real Madrid","Tottenham","Nápoles","B. Dortmund","O. Lyon","Chelsea","Atalanta","Atético de Madrid"};
+        int[]eqp1Guardado=new int[8];
+        int[]eqp2Guardado=new int [8];
+        int equipFinal1=0,equipFinal2=0;
+        int i,j;
+
+        for ( i = 0; i < equipos1.length; i++) {
+            eqp1Guardado[i]=(int)(Math.random()*equipos1.length);
+            eqp2Guardado[i]=(int) (Math.random()*equipos1.length);
+            equipFinal1=eqp1Guardado[i];
+
+            System.out.println();
+
+            for (int k = 0; k < eqp1Guardado.length; k++) {
+               // if (equipos1.equalsIgnoreCase()
+                {
+                }
+
+            }
+
+        }
+
+
+        for ( i = 0; i < equipos1.length; i++) {
+            if () {
+            }
+            System.out.println(eqp1Guardado[i]);
+            System.out.println(eqp2Guardado[j]);
+
+            //equipos1[equipFinal1]
+
+        }
+
+
+
+
+    }
+}
---
+++ TEMA 4/T04_ARRAY/src/ArrayList/Ejercicio12Invisible.java
@@ -0,0 +1,7 @@
+package ArrayList;
+
+public class Ejercicio12Invisible {
+    //si el nro es impar da error
+
+
+}
