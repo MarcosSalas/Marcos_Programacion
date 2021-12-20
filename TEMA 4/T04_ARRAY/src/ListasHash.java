318f2d2b224d891353d65818c221518130de8748
AlvaroSR-dev <alvarosanchezruiz3@gmail.com>
2021-12-20 10:04:26 +0100

    q

--- TEMA 4/T04_ARRAY/src/ListasHash.java
+++ TEMA 4/T04_ARRAY/src/ListasHash.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class ListasHash {
@@ -19,19 +20,44 @@ public class ListasHash {
         //listaElementos.get("4");//pregunto por la clave 4 y me va a devolver un String Array
 
         //TENDRIA QUE CASTEAR
-        String[] elementoEncontrado=(String[]) listaElementos.get("4");//para igualar a una variable reutilizarla tengo q comentar el get de arriba
+    //    String[] elementoEncontrado=(String[]) listaElementos.get("4");//para igualar a una variable reutilizarla tengo q comentar el get de arriba
         //en cada vuelta con el for me encuentro un Srtring y no un Array de String
-        elementoEncontrado[0]="Nuevo valor";// con el metodo del GET OBTENGO EL ARRAY ,A PARTIR DE ESE MOMENTO EL 1ER ELEMENTO QUE TENGO EN EL ARRAY CAMBIO EL VALOR,ES DECIR Cosa1,toma el valor de Nuevo valor
+   //     elementoEncontrado[0]="Nuevo valor";// con el metodo del GET OBTENGO EL ARRAY ,A PARTIR DE ESE MOMENTO EL 1ER ELEMENTO QUE TENGO EN EL ARRAY CAMBIO EL VALOR,ES DECIR Cosa1,toma el valor de Nuevo valor
       /*  for (String item:elementoEncontrado) { //Recorro
             System.out.println(item);
         }*/
 
         //BORRAR 1 ELEMENTO
 
-        listaElementos.remove("1");//Eliminararia elemento1 y la key- es decir que si luego quiero ingresar algo en la key "1", tendría que volver a crearla
+    //    listaElementos.remove("1");//Eliminararia elemento1 y la key- es decir que si luego quiero ingresar algo en la key "1", tendría que volver a crearla
      //   listaElementos.remove("7");//como es booleano me va a devolver null porque en el 7 no tenemos nada
 
 //puedo meter una hashtable dentro de otra hashtabe
+
+        //lista.PUT Y NO ADD para añadir en hashtable.
+        //lista.Get para preguntar x una key y te busque los datos.
+        //lista.containkey(m7) devuelve boolean(objt)
+        //if(lista.containkey(m5)), pregunto si contiene la matricula, si no la contiene agrego los datos
+        //if(lista.get(m7)!=null))
+        //lista.remove(m4) elimina
+
+
+        //                              *****PARA IMPRIMIR***
+        //int cont=0; NO SE USA ESTE POR QUE VA POR CLAVE NO POR POSICIONES
+       // for (Object ver : elementoEncontrado ) NO LO PUEDO USAR PORQUE NO VA POR POSICIONES
+
+                        // ELEMENTS() DEVUELVE TODOS LOS ELEMENTOS, SIRVE PARA IMPRIMIR EN HASH TODAS LAS POS,LOS ELEMENTS
+                       // keys() devuelve enumeration , x eso a Enumeration var le asigno para las claves, pero preciso los elementos.
+
+        System.out.println("Listado completo");
+        Enumeration <Object>  /*(lo tipo del valor que tenga la key)*/    listaClaves =listaElementos.keys(); //LISTA DE TODAS LAS CLAVES
+        while (listaClaves.hasMoreElements())/*devuelve un true*, si pogo listaClaves.hasMoreElements()==true es redundante/)*/ {
+        //hay elemento siguiente
+            Object clave =listaClaves.nextElement();
+            Object elemento=listaElementos.get(clave);
+            System.out.println(elemento);  //la impresión sale de abajo para arriba, imprimo el objeto, seria como la pocisión en el arra
+
+        }
 
 
     }
