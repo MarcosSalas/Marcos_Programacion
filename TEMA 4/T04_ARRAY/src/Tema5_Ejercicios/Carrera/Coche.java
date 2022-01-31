98e94442e983eaaa0cec8fe99d3c6f2133030267
MarcosSalas <92918284+MarcosSalas@users.noreply.github.com>
2022-02-01 00:38:52 +0100

    s

--- TEMA 4/T04_ARRAY/src/Tema5_Ejercicios/Carrera/Coche.java
+++ TEMA 4/T04_ARRAY/src/Tema5_Ejercicios/Carrera/Coche.java
@@ -4,52 +4,99 @@ public class Coche {
 
     //variable
 
-    private int caballos,velocidad;
-    private String matricula,modelo;
+    private int caballos, velocidad;
+    private String matricula, modelo;
     private double kilometros;
 
 
     //constructor
 
-    public Coche(){
-        caballos=0;
-        velocidad=0;
-        matricula="0000AAA";
-        modelo="sin especificar";
-        kilometros=0.0;
+    public Coche() {
+        caballos = 0;
+        velocidad = 0;
+        matricula = "0000AAA";
+        modelo = "sin especificar";
+        kilometros = 0.0;
     }
 
-    public Coche(String modelo,String matricula,int caballos){
-        this.modelo=modelo;
-        this.matricula=matricula;
-        this.caballos=caballos;
+    public Coche(String modelo, String matricula, int caballos) {
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.caballos = caballos;
 
     }
     //metodo
 
-    public void acelerar(int incremento){
-        velocidad+=incremento;  // en este caso acumulo y sumo a la vez
-        kilometros+=velocidad*(caballos)*(int)(Math.random()*11)+1;
-        final int VELMAX =180;
-        if (velocidad>VELMAX) {
-            velocidad=180;
+    public void acelerar(int incremento) {
+        velocidad += incremento;  // en este caso acumulo y sumo a la vez
+        kilometros += velocidad * (caballos) * (int) (Math.random() * 11) + 1;
+
+        final int VELMAX = 180;
+        if (velocidad > VELMAX) {
+            velocidad = 180;
             System.out.println("Velocidad excedida de 180");
         }
     }
 
-    public void frenar(int frenar){
-        velocidad-=frenar;
+    public void frenar(int frenar) {
+        velocidad -= frenar;
         final int VELMIN = 0;
-        if (velocidad<VELMIN) {
-            velocidad=0;
+        if (velocidad < VELMIN) {
+            velocidad = 0;
             System.out.println("Velocidad debajo de 0");
         }
-
-
     }
 
+    public void velocidadCero() {
+        velocidad = 0;
+    }
 
+    public void resetear() {
+        velocidad = 0;
+        kilometros = 0;
+    }
 
+    public void mostrarDatos() {
+        System.out.println("Matrícula " + matricula + ", Modelo " + modelo + ", CV " + caballos + ", Velocidad " + velocidad + ", Kilómetros " + kilometros);
+    }
 
+    public int getCaballos() {
+        return caballos;
+    }
 
+    public void setCaballos(int caballos) {
+        this.caballos = caballos;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getKilometros() {
+        return kilometros;
+    }
+
+    public void setKilometros(double kilometros) {
+        this.kilometros = kilometros;
+    }
 }
--- TEMA 4/T04_ARRAY/src/Tema5_Ejercicios/Carrera/Entrada.java
+++ TEMA 4/T04_ARRAY/src/Tema5_Ejercicios/Carrera/Entrada.java
@@ -3,9 +3,31 @@ package Tema5_Ejercicios.Carrera;
 public class Entrada {
     public static void main(String[] args) {
 
+        Coche coche = new Coche();
+        Coche coche1 = new Coche("Astra", "KRE4833", 110);
 
 
+        coche.setModelo("leon");
+        coche.setMatricula("KEW4286");
+        coche.setCaballos(125);
+        coche.setVelocidad((int) (Math.random() * (31 - 10) + 10));    //      System.out.println("Números generados entre 5 y 20, con decimales (sin incluir el 5 y el 20)");
+        // System.out.println("coche vel " + coche.getVelocidad());  //      int numAleatorio=Math.random()*(20-5)+5;
+        coche1.setVelocidad((int) (Math.random() * (31 - 10) + 10));
+        // System.out.println("coche B velocidad " + coche1.getVelocidad());
+        coche.mostrarDatos();
+        coche1.mostrarDatos();
 
+        coche.acelerar(20);
+        System.out.println("velocidad "+ coche.getKilometros());
+        coche1.acelerar(20);
+        System.out.println("velocidad coche 2 " + coche1.getKilometros());
+
+        if (coche.getKilometros() > coche1.getKilometros()) {
+            System.out.println("el ganador es coche 1, " + coche.getModelo() + ", Matrícula " + coche.getMatricula() + ", con " + coche.getKilometros() + " recorridos");
+        } else {
+            System.out.println("el ganador es coche 2, " + coche1.getModelo() + ", Matrícula " + coche1.getMatricula() + ", con " + coche1.getKilometros() + " recorridos");
+
+        }
 
 
     }
