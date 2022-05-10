5e643449182c62c827aa8f22ea712ed62bd68a01
MarcosSalas <92918284+MarcosSalas@users.noreply.github.com>
2022-05-10 02:50:23 +0200

    s

--- TEMA_9_DB/src/main/java/Main.java
+++ TEMA_9_DB/src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        //en el caso que no me llegara a tomar el main como Clase tengo que poner en el pom reload proyect de nuevo
+
         System.out.println("sa");
         
     }
