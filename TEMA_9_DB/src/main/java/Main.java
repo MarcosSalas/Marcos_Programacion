ffd77151d489cfdfb9aa2ea7ef6072fa5d6d00a0
MarcosSalas <92918284+MarcosSalas@users.noreply.github.com>
2022-05-10 05:39:41 +0200

    s

--- TEMA_9_DB/src/main/java/Main.java
+++ TEMA_9_DB/src/main/java/Main.java
@@ -1,11 +1,48 @@
-import java.sql.Connection;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
         //en el caso que no me llegara a tomar el main como Clase tengo que poner en el pom reload proyect de nuevo, si nl crear una clase dentro de test en el pom reload proyect y borramos la clase creada
 
-        System.out.println("sa");
-        /*Co*/
+      //## CONECTARSE A BD
+        //0)c-Reemplazamos valores
+        String host="127.0.0.1:3306";//este es para todos igual (el puerto 3306 tmb)
+        String dtbs="colegio"; // nombre de bd ya creada
+       // String user="root";
+        String user="programacion";
+        String pass="programacion";
+       // String pass=""; //todos tenemos root con contraseña vacía,pero habría que ponerle
+        Connection conn=null;//3)b
+        //### 5 UNA VEZ CREADO T@DO Y NO DAR FALLO PUEDO EMPEZAR A UTILIZARLA ,MEDIANTE EL STATEMENT Y EL RESURCE
+        Statement st = conn.createStatement(); //No puedo EJECUTAR UN Statement si no tengo previamente una coneccion //me va a pedir trycathc, lo vemos hoy
+        ResultSet rs; //Lo puedo ir recorriendo mediante sus métodos..ej abajo
+        //rs.next();
+        //rs.findColumn();
+        PreparedStatement ps;
+        ps.//Aquí la comprobación de tipos te la doy antes de ejecutar la query
+
+
+
+        // 0)b EN LA PARTE DE ARRIBA REEMPLAZAMOS ESTOS VALORES
+        //0)a- copiamos y pegamos lo de abajo
+        String newConnectionURL = "jdbc:mysql://" + host + "/" + dtbs + "?" + "user=" + user + "&password=" + pass;
+        try {
+            // 3)a para sacar el error de conn, me Creo arriba un Obj de tipo Connection=conn y lo igualo a null,para poder trabajarlo en el trycatch
+            conn = (Connection) DriverManager.getConnection(newConnectionURL);//1)DriverManager altEnter import  2)getConection TryCathc(con el finally etc)
+            System.out.println(conn.getCatalog()); //4 una vez cargado t@do si hago el print de conn.getCatalog me da colegio,el nombre de mi bd
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (conn != null) {
+                    conn.close();//para poder cerrarlo primero tengo que crearlo al conn
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+        // Connection connection =
         
     }
 }
